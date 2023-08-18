86839297434fb2c4ed222ede61629b297b8aad7e
Jared Scholz <jaredsc108@gmail.com>
2023-08-18 13:55:19 +1200

    Update code according to Play version 2.8.20

--- playprovider/src/main/java/com/mindscapehq/raygun4java/play2/RaygunPlayJavaRequestHeaderMessage.java
+++ playprovider/src/main/java/com/mindscapehq/raygun4java/play2/RaygunPlayJavaRequestHeaderMessage.java
@@ -3,6 +3,7 @@ package com.mindscapehq.raygun4java.play2;
 import play.mvc.Http.RequestHeader;
 
 import java.util.logging.Logger;
+import java.util.stream.Collectors;
 
 public class RaygunPlayJavaRequestHeaderMessage extends RaygunPlayRequestMessage {
     public RaygunPlayJavaRequestHeaderMessage(RequestHeader javaRequestHeader) {
@@ -16,7 +17,10 @@ public class RaygunPlayJavaRequestHeaderMessage extends RaygunPlayRequestMessage
                 queryString = flattenMap(javaRequestHeader.queryString());
             }
 
-            headers = flattenMap(javaRequestHeader.headers());
+            headers = flattenMap(javaRequestHeader.getHeaders().asMap().entrySet().stream().collect(Collectors.toMap(
+                    entry -> entry.getKey(),
+                    entry -> entry.getValue().toArray(new String[0])
+            )));
         } catch (Throwable t) {
             Logger.getLogger("Raygun4Java-Play2").info("Couldn't get all request params: " + t.getMessage());
         }
